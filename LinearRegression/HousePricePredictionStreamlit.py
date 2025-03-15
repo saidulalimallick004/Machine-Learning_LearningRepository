9e6196d58164988b1c7f21cefd3a868258cfeef8
saidulalimallick004 <saidulalimallick1@gmail.com>
2025-03-15 14:19:04 +0530

    LinearRegression     1. House Price Prediction Model

---
+++ LinearRegression/HousePricePredictionStreamlit.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pickle,joblib
+
+
+st.title('The House Price Prediction Model: ')
+
+a=st.number_input("Enter Area(sq.ft.):",value=1500)
+b=st.number_input("Enter No of Bedrooms:",value=4)
+c=st.number_input("Enter No of Bathrooms:",value=3)
+
+with open('SavedVariable/Reverse_mapping.plk','rb') as f:
+    reverse_mapping=dict(pickle.load(f))
+City_list1= list(reverse_mapping.keys())
+
+d=st.selectbox(
+    "Select City:",City_list1
+)
+d=reverse_mapping.get(d,None)
+UserInput=[a,b,c,d] 
+
+if st.button("Predict", type='secondary'):
+    HousePricePredictionModel=joblib.load('SavedModel/HousePricePredictionModel.plk')
+    Pred_price=HousePricePredictionModel.predict([UserInput])
+
+    st.write("---------------------------------------------------------------------")
+    st.write(f"""
+      Area:{UserInput[0]}\n
+      No of Bedroom:{UserInput[1]}\n
+      No of Bathroom:{UserInput[2]}\n
+      City:{UserInput[3]}\n
+    
+      Price(INR) = Rs.{Pred_price[0]:.2f}
+      -------------------------------------
+""")
+    
+
+#RUN IN Terminal:
+#      streamlit must needed 
+##    "streamlit run HousePricePredictionStreamlit.py"

36a6da7893e41e7de8e254e23cec9d5948362c21
saidulalimallick004 <saidulalimallick1@gmail.com>
2025-03-18 19:06:59 +0530

    Version 1.0.0

--- LinearRegression/House_Price_Prediction/HousePricePredictionStreamlit.py
+++ LinearRegression/House_Price_Prediction/HousePricePredictionStreamlit.py
@@ -1,36 +1,54 @@
 import streamlit as st
 import pickle,joblib
+from sklearn.linear_model import LinearRegression
 
 
 st.title('The House Price Prediction Model: ')
+
+with open("resources/model_currectness_value.plk",'rb') as x:
+    detail=dict(pickle.load(x))
+
+with st.expander("Click to see Model details"):
+    st.write("Model Detail:")
+    st.write("R2 Score:",detail['model_r2_score'])
+    st.write("Mean Absolute Error Score:",detail['model_mae'])
+    st.write("Mean Squere Error Score:",detail['model_mse'])
+
 
 a=st.number_input("Enter Area(sq.ft.):",value=1500)
 b=st.number_input("Enter No of Bedrooms:",value=4)
 c=st.number_input("Enter No of Bathrooms:",value=3)
 
 
-Option_PATH = "resources/Reverse_mapping.pkl"
-with open(Option_PATH,'rb') as f:
+
+with open("resources/Reverse_mapping.plk",'rb') as f:
     reverse_mapping=dict(pickle.load(f))
 City_list1= list(reverse_mapping.keys())
 
 d=st.selectbox(
     "Select City:",City_list1
 )
+
+city=d
 d=reverse_mapping.get(d,None)
 UserInput=[a,b,c,d] 
 
+
+
 if st.button("Predict", type='secondary'):
-    MODEL_PATH = 'resources/HousePricePredictionModel.plk'
-    HousePricePredictionModel=joblib.load(MODEL_PATH)
+
+    HousePricePredictionModel=joblib.load('resources/HousePricePredictionModel.plk')
     Pred_price=HousePricePredictionModel.predict([UserInput])
+
+    
+
 
     st.write("---------------------------------------------------------------------")
     st.write(f"""
-      Area:{UserInput[0]}\n
-      No of Bedroom:{UserInput[1]}\n
-      No of Bathroom:{UserInput[2]}\n
-      City:{UserInput[3]}\n
+      Area: {UserInput[0]}\n
+      No of Bedroom: {UserInput[1]}\n
+      No of Bathroom: {UserInput[2]}\n
+      City: {city}\n
     
       Price(INR) = Rs.{Pred_price[0]:.2f}
       -------------------------------------
